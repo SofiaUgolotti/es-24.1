7d1fab37a2730f5bd5dd7af70ef48ecf753d3940
SofiaUgolotti <149767666+SofiaUgolotti@users.noreply.github.com>
2023-11-28 19:14:12 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using es_24._1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("inserisci lato quadrato");
+        float lato= float.Parse (Console.ReadLine());   
+        Quadrato quadrato = new Quadrato();
+        quadrato.Lato = lato;
+        float superficie = quadrato.CalcolaSuperficie();
+        Console.WriteLine($"la superficie del quadrato con lato {lato} è: {superficie}");
+        Console.ReadLine ();
+    }
+}
